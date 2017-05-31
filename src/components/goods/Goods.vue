ab81fbdf07c3a564587b4a6e28e23647d151dfe4
Hillhe <15619155282@126.com>
2017-05-31 23:39:54 +0800

    修改Goods.vue 添加滚动

--- src/components/goods/Goods.vue
+++ src/components/goods/Goods.vue
@@ -1,33 +1,33 @@
 <template>
   <div class="goods">
-		<div class="menu-wrapper">
+		<div class="menu-wrapper" v-el:menu-wrap>
 			<ul>
-				<li v-for="item in goods" class="menu-item">
+				<li v-for="item in goods" class="menu-item" :class="{'currentIndex': currentIndex===$index}">
 					<span class="text">
 						<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
 					</span>
 				</li>
 			</ul>
 		</div>
-		<div class="goods-wrapper">
+		<div class="goods-wrapper" v-el:goods-wrap>
 			<ul>
-				<li v-for="item in goods" class="food-list">
+				<li v-for="item in goods" class="good-list good-list-hook">
 					<h1 class="title">{{item.name}}</h1>
 					<ul>
 						<li v-for="food in item.foods" class="food-item">
 							<div class="icon">
-								<img :src="food.icon" alt="">
+								<img width="57" height="57" :src="food.icon" alt="">
 							</div>
 							<div class="content">
 								<h2 class="name">{{food.name}}</h2>
 								<p class="desc">{{food.description}}</p>
 								<div class="extra">
-									<span>月售{{food.sellCount}}份</span>
+									<span class="count">月售{{food.sellCount}}份</span>
 									<span>好评率{{food.rating}}%</span>
 								</div>
 								<div class="price">
-									<span>￥{{food.price}}</span>
-									<span v-show="food.oldPrice">{{food.oldPrice}}</span>
+									<span class="now">￥{{food.price}}</span>
+									<span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
 								</div>
 							</div>
 						</li>
@@ -39,6 +39,7 @@
 </template>
 
 <script type="text/ecmascript-6">
+	import BScroll from 'better-scroll'
 	const ERR_OK = 0
 	export default {
 		props: {
@@ -48,7 +49,21 @@
 		},
 		data() {
 			return {
-				goods: []
+				goods: [],
+				listHeight: [],
+				scrollY: 0
+			}
+		},
+		computed: {
+			currentIndex() {
+				for (let i = 0; i < this.listHeight.length; i++) {
+					let height1 = this.listHeight[i]
+					let height2 = this.listHeight[i + 1]
+					if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+						return i
+					}
+				}
+				return 0
 			}
 		},
 		created() {
@@ -57,8 +72,33 @@
 				response = response.body
 				if (response.code === ERR_OK) {
 					this.goods = response.msg
+					this.$nextTick(() => {
+						this._initScroll()
+						this.calculateHeight()
+					})
 				}
 			})
+		},
+		methods: {
+			_initScroll() {
+				this.menuScroll = new BScroll(this.$els.menuWrap, {})
+				this.goodsScroll = new BScroll(this.$els.goodsWrap, {
+					probeType: 3
+				})
+				this.goodsScroll.on('scroll', (pos) => {
+					this.scrollY = Math.abs(Math.round(pos.y))
+				})
+			},
+			calculateHeight() {
+				let goodList = this.$els.goodsWrap.getElementsByClassName('good-list-hook')
+				let height = 0
+				this.listHeight.push(height)
+				for (let i = 0; i < goodList.length; i++) {
+					let item = goodList[i]
+					height += item.clientHeight
+					this.listHeight.push(height)
+				}
+			}
 		}
 }
 </script>
@@ -76,13 +116,20 @@
 			flex: 0 0 80px
 			width: 80px
 			background: #f3f5f7
-			overflow: auto
 			.menu-item
 				display: table
 				height: 54px
 				width: 56px
 				padding: 0 12px
 				line-height: 14px
+				&.currentIndex
+					position: relative
+					margin-top: -1px
+					z-index: 10
+					background: #fff
+					font-weight: 700
+					.text
+						border-none()
 				.icon
 					display: inline-block
 					vertical-align: top
@@ -106,8 +153,59 @@
 					width: 56px
 					vertical-align: middle
 					font-size: 12px
-					border-bottom: 1px solid rgba(7,17,27,0.1)
+					border-1px(rgba(7,17,27,0.1))
 		.goods-wrapper
 			flex: 1
-			overflow: auto
+			.title
+				height: 26px
+				padding-left: 14px
+				font-size: 12px
+				color: rgb(147,153,159)
+				font-weight: 700
+				line-height: 26px
+				background: #f3f5f7
+				border-left: 2px solid #d9dde1
+			.food-item
+				display: flex
+				margin: 18px
+				padding-bottom: 18px
+				border-1px(rgba(7,17,27,0.1))
+				&:last-child
+					border-none()
+					margin-bottom: 0
+				.icon
+					flex: 0 0 57px
+					margin-right: 10px
+				.content
+					flex: 1
+					.name
+						margin: 2px 0 8px 0
+						height: 14px
+						line-height: 14px
+						font-size: 14px
+						color: rgb(7,17,27)
+					.desc,.extra
+						line-height: 14px
+						font-size: 10px
+						color: rgb(147,153,159)
+					.desc
+						margin-bottom: 8px
+					.extra
+						.count
+							margin-right: 12px
+					.price
+						font-weight: 700
+						line-height: 24px
+						.now
+							margin-right: 18px
+							font-size: 14px
+							color: rgb(240, 20, 20)
+						.old
+							text-decoration: line-through
+							font-size: 10px
+							color: rgb(147,153,159)
+					.name,.desc
+						white-space: nowrap
+						overflow: hidden
+						text-overflow: ellipsis
 </style>
