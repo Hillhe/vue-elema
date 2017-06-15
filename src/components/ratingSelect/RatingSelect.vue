f255f8ecdbaee4f53e469b7616be219d21e8736a
Hillhe <15619155282@126.com>
2017-06-15 22:40:08 +0800

    继续编写Food, RatingSelect组件， 添加data.js

--- src/components/ratingSelect/RatingSelect.vue
+++ src/components/ratingSelect/RatingSelect.vue
@@ -1,20 +1,20 @@
 <template>
 	<div class="ratingSelect">
 		<div class="ratingType">
-			<span class="block positive" :class="{'active':selectType===2}">{{desc.all}}<span class="count">47</span></span>
-			<span class="block positive" :class="{'active':selectType===0}">{{desc.positive}}<span class="count">18</span></span>
-			<span class="block negative" :class="{'active':selectType===1}">{{desc.negative}}<span class="count">10</span></span>
+			<span class="block positive" :class="{'active':selectType===2}" @click="select(2,$event)">{{desc.all}}<span class="count">{{ratings.length}}</span></span>
+			<span class="block positive" :class="{'active':selectType===0}" @click="select(0,$event)">{{desc.positive}}<span class="count">{{positives.length}}</span></span>
+			<span class="block negative" :class="{'active':selectType===1}" @click="select(1,$event)">{{desc.negative}}<span class="count">{{negatives.length}}</span></span>
 		</div>
-		<div class="switch">
+		<div class="switch" :class="{'on':onlyContent}" @click="toggleContent">
 			<span class="icon-check_circle"></span>
-			<span>只看有内容的评价</span>
+			<span class="text">只看有内容的评价</span>
 		</div>
 	</div>
 </template>
 
 <script type="text/ecmascript-6">
-	// const POSITIVE = 0
-	// const NEGATIVE = 1
+	const POSITIVE = 0
+	const NEGATIVE = 1
 	const ALL = 2
 	export default {
 		props: {
@@ -41,6 +41,34 @@
 						negative: '不满意'
 					}
 				}
+			}
+		},
+		methods: {
+			select(type, event) {
+				if (!event._constructed) {
+					return
+				}
+				this.selectType = type
+				this.$dispatch('ratingtype.select', type)
+			},
+			toggleContent(event) {
+				if (!event._constructed) {
+					return
+				}
+				this.onlyContent = !this.onlyContent
+				this.$dispatch('content.toggle', this.onlyContent)
+			}
+		},
+		computed: {
+			positives() {
+				return this.ratings.filter((rating) => {
+					return rating.rateType === POSITIVE
+				})
+			},
+			negatives() {
+				return this.ratings.filter((rating) => {
+					return rating.rateType === NEGATIVE
+				})
 			}
 		}
 	}
@@ -75,4 +103,20 @@
 					background: rgba(77,85,93,0.2)
 					&.active
 						background: rgb(77,85,93)
+		.switch
+			padding: 12px 18px
+			line-height: 24px
+			border-bottom: 1px solid rgba(7,17,27,0.1)
+			color: rgb(147,153,159)
+			font-size: 0
+			&.on
+				.icon-check_circle
+					color: #00c850
+			.icon-check_circle
+				display: inline-block
+				vertical-align: top
+				margin-right: 4px
+				font-size: 24px
+			.text
+				font-size: 12px
 </style>
